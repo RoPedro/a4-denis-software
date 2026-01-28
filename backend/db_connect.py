15354b0d1827962c10044ca528356768e9525622
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2026-01-28 11:11:49 -0300

    feat: checks if db exists
    uses sqalchemy_utils to check if db exists and logs the result.

--- backend/db_connect.py
+++ backend/db_connect.py
@@ -1,13 +1,18 @@
 import logging
 from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists
 from dotenv import load_dotenv
 import os
 
 logger = logging.getLogger(__name__)
 
 load_dotenv()
+# URL to connect to postgres
 db_url = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 
 logger.debug(f"DB URL: {db_url}")
 
 engine = create_engine(db_url)
+if not database_exists(engine.url):
+    logger.error("Database does not exist.")
+    
